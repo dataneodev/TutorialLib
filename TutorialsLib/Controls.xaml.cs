657a2289a028261c2dac14cd07b061859a329715
dataneo <bprv6qazvbwni@gmail.com>
2021-01-16 21:45:39 +0100

    Dodaj pliki projektów.

---
+++ TutorialsLib/Controls.xaml.cs
@@ -0,0 +1,64 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace TutorialsLib
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Controls.xaml
+    /// </summary>
+    public partial class Controls : UserControl
+    {
+        readonly MainWindow parent;
+        LibVLC _libVLC;
+        MediaPlayer _mediaPlayer;
+
+        public Controls(MainWindow Parent)
+        {
+            parent = Parent;
+
+            InitializeComponent();
+
+            // we need the VideoView to be fully loaded before setting a MediaPlayer on it.
+            parent.VideoView.Loaded += VideoView_Loaded;
+            PlayButton.Click += PlayButton_Click;
+            StopButton.Click += StopButton_Click;
+            Unloaded += Controls_Unloaded;
+        }
+
+        private void Controls_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _mediaPlayer.Stop();
+            _mediaPlayer.Dispose();
+            _libVLC.Dispose();
+        }
+
+        private void VideoView_Loaded(object sender, RoutedEventArgs e)
+        {
+            _libVLC = new LibVLC(enableDebugLogs: true);
+            _mediaPlayer = new MediaPlayer(_libVLC);
+
+            parent.VideoView.MediaPlayer = _mediaPlayer;
+        }
+
+        void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (parent.VideoView.MediaPlayer.IsPlaying)
+            {
+                parent.VideoView.MediaPlayer.Stop();
+            }
+        }
+
+        void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!parent.VideoView.MediaPlayer.IsPlaying)
+            {
+                using (var media = new Media(_libVLC, new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")))
+                    parent.VideoView.MediaPlayer.Play(media);
+            }
+        }
+
+    }
+}
---
+++ TutorialsLib/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TutorialsLib
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly Controls _controls;
+        public MainWindow()
+        {
+            Core.Initialize();
+            InitializeComponent();
+
+            _controls = new Controls(this);
+            VideoView.Content = _controls;
+
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            VideoView.Dispose();
+        }
+
+    }
+}
