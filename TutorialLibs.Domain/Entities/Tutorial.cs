6b3bbc59906ff55120b3683e282e192c96eb53fb
dataneo <BPrv6QazVbwni@gmail.com>
2021-04-18 17:46:43 +0200

    update

--- TutorialLibs.Domain/Entities/Tutorial.cs
+++ TutorialLibs.Domain/Entities/Tutorial.cs
@@ -6,7 +6,7 @@ namespace TutorialLibs.Domain.Entities
     public class Tutorial : BaseEntity, IAggregateRoot
     {
         public string Name { get; set; }
-        public string FolderPath { get; set; }
+        public string BasePath { get; set; }
         public Episode[] Episodes { get; set; }
     }
 }
