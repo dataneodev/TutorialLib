c689567cf6b0b0b16d852234c438fef9278a1119
dataneo <BPrv6QazVbwni@gmail.com>
2021-05-21 02:35:40 +0200

    DateTimeProivder

---
+++ dataneo.TutorialLibs.Domain/Services/DateTimeProivder.cs
@@ -0,0 +1,10 @@
+﻿using dataneo.TutorialLibs.Domain.Interfaces;
+using System;
+
+namespace dataneo.TutorialLibs.Domain.Services
+{
+    public sealed class DateTimeProivder : IDateTimeProivder
+    {
+        public DateTime Now => DateTime.Now;
+    }
+}
