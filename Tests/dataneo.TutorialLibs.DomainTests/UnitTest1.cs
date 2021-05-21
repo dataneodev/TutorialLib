7fedbebd509e7d95062e1125daaff3bd566c0b20
dataneo <BPrv6QazVbwni@gmail.com>
2021-05-21 23:23:36 +0200

    add dataneo.TutorialLibs.DomainTests

---
+++ Tests/dataneo.TutorialLibs.DomainTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace dataneo.TutorialLibs.DomainTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
