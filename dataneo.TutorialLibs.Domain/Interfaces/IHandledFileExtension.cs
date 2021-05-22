fa577eeab4d81f115b0dc9a8563087559c3572c0
dataneo <BPrv6QazVbwni@gmail.com>
2021-05-22 20:44:23 +0200

    add IHandledFileExtension interface

---
+++ dataneo.TutorialLibs.Domain/Interfaces/IHandledFileExtension.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace dataneo.TutorialLibs.Domain.Interfaces
+{
+    public interface IHandledFileExtension
+    {
+        HashSet<string> HandledFileExtensions { get; }
+    }
+}
