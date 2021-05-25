48cc60ed0f46967769a6e69ed46865e885e91010
dataneo <BPrv6QazVbwni@gmail.com>
2021-05-25 22:58:27 +0200

    add TutorialHeaderDto

---
+++ dataneo.TutorialLibs.Domain/DTO/TutorialHeaderDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace dataneo.TutorialLibs.Domain.DTO
+{
+    public sealed class TutorialHeaderDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public TimeSpan TotalTime { get; set; }
+        public TimeSpan TimePlayed { get; set; }
+        public short Episodes { get; set; }
+        public short PlayedEpisodes { get; set; }
+        public short TotalEpisodes { get; set; }
+        public DateTime DateAdd { get; set; }
+        public float TotalSizeMB { get; set; }
+    }
+}
