071cd5c77b6954e89d388c6bfc309790fde1041c
dataneo <BPrv6QazVbwni@gmail.com>
2021-05-29 00:12:40 +0200

    RatingControl

--- dataneo.TutorialsLib.WPF/UI/Main/RatingControl.xaml.cs
+++ dataneo.TutorialsLib.WPF/UI/Main/RatingControl.xaml.cs
@@ -1,73 +1,91 @@
-﻿using System.Windows.Controls;
+﻿using dataneo.TutorialLibs.Domain.Enums;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace dataneo.TutorialsLib.WPF.UI.Main
 {
     /// <summary>
     /// Interaction logic for RatingStars.xaml
     /// </summary>
-    public partial class RatingStars : UserControl
+    public partial class RatingControl : UserControl
     {
-        private const string StarEmpty = @"/dataneo.TutorialsLib.WPF;component/Image/star.png";
-        public RatingStars Rating { get; set; }
+        private static Brush Star = Brushes.Yellow;
+        private static Brush StarEmpty = Brushes.Gray;
 
-        public RatingStars()
+        public static readonly DependencyProperty RatingProperty =
+         DependencyProperty.Register("Rating", typeof(RatingStars), typeof(StarControl), new
+            PropertyMetadata("", new PropertyChangedCallback(OnStarredChanged)));
+
+        public RatingStars Rating
+        {
+            get { return (RatingStars)GetValue(RatingProperty); }
+            set { SetValue(RatingProperty, value); }
+        }
+
+        private static void OnStarredChanged(DependencyObject d,
+           DependencyPropertyChangedEventArgs e)
+        {
+            RatingControl UserControl1Control = d as RatingControl;
+            UserControl1Control.OnSetTextChanged(e);
+        }
+
+        private void OnSetTextChanged(DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        public RatingControl()
         {
             InitializeComponent();
         }
 
         private void SetRatingStatus(RatingStars ratingStars)
         {
+            var value = (byte)ratingStars;
 
+            if (value > 0)
+                scStar1.pathStart.Fill = Star;
+            else
+                scStar1.pathStart.Fill = StarEmpty;
+
+            if (value > 1)
+                scStar2.pathStart.Fill = Star;
+            else
+                scStar2.pathStart.Fill = StarEmpty;
+
+            if (value > 2)
+                scStar3.pathStart.Fill = Star;
+            else
+                scStar3.pathStart.Fill = StarEmpty;
+
+            if (value > 3)
+                scStar4.pathStart.Fill = Star;
+            else
+                scStar4.pathStart.Fill = StarEmpty;
+
+            if (value > 4)
+                scStar5.pathStart.Fill = Star;
+            else
+                scStar5.pathStart.Fill = StarEmpty;
         }
 
-        private void imgStart2_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
+        private void scStar1_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+            => this.SetRatingStatus(RatingStars.OneStar);
 
-        }
+        private void scStar_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+            => this.SetRatingStatus(this.Rating);
 
-        private void imgStart2_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
-        {
+        private void scStar2_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+            => this.SetRatingStatus(RatingStars.TwoStars);
 
-        }
+        private void scStar3_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+            => this.SetRatingStatus(RatingStars.ThreeStart);
 
-        private void imgStart1_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
+        private void scStar4_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+            => this.SetRatingStatus(RatingStars.FourStars);
 
-        }
-
-        private void imgStart1_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-
-        }
-
-        private void imgStart3_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-
-        }
-
-        private void imgStart4_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-
-        }
-
-        private void imgStart5_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-
-        }
-
-        private void imgStart3_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-
-        }
-
-        private void imgStart4_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-
-        }
-
-        private void imgStart5_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-
-        }
+        private void scStar5_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+            => this.SetRatingStatus(RatingStars.FiveStars);
     }
 }
