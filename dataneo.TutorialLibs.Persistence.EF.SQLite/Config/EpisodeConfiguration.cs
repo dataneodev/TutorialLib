b2cec0312362eb434cb2a14de552e20e26ea11fc
dataneo <BPrv6QazVbwni@gmail.com>
2021-06-01 21:35:06 +0200

    ef core init

---
+++ dataneo.TutorialLibs.Persistence.EF.SQLite/Config/EpisodeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataneo.TutorialLibs.Persistence.EF.SQLite.Config
+{
+    public class EpisodeConfiguration
+    {
+        
+    }
+}
---
+++ dataneo.TutorialLibs.Persistence.EF.SQLite/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataneo.TutorialLibs.Persistence.EF.SQLite.Context
+{
+    public class ApplicationDbContext : DbContext
+
+    {
+        
+    }
+}
