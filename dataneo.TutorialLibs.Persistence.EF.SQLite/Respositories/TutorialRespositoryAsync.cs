4568d0bf4969d60f27f3a235c9ddc3f34a0532cf
dataneo <BPrv6QazVbwni@gmail.com>
2021-06-11 02:33:34 +0200

    respository update

---
+++ dataneo.TutorialLibs.Persistence.EF.SQLite/Respositories/TutorialRespositoryAsync.cs
@@ -0,0 +1,21 @@
+﻿using CSharpFunctionalExtensions;
+using dataneo.TutorialLibs.Domain.DTO;
+using dataneo.TutorialLibs.Domain.Entities;
+using dataneo.TutorialLibs.Domain.Interfaces.Respositories;
+using dataneo.TutorialLibs.Persistence.EF.SQLite.Context;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dataneo.TutorialLibs.Persistence.EF.SQLite.Respositories
+{
+    public class TutorialRespositoryAsync : EfRepository<Tutorial>, ITutorialRespositoryAsync
+    {
+        public TutorialRespositoryAsync(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        { }
+
+        public Task<Maybe<TutorialHeaderDto>> GetTutorialHeaderDtoByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return Maybe<TutorialHeaderDto>.None;
+        }
+    }
+}
