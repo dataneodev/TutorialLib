2185cc2a79fd1aaf5ebf64824ef91ad822d6d810
dataneo <BPrv6QazVbwni@gmail.com>
2021-10-16 17:35:04 +0200

    rem

--- dataneo.TutorialLibs.Domain/Tutorials/Services/DeconstructionPath/FolderWithFIles.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace dataneo.TutorialLibs.Domain.Tutorials.Services
-{
-    internal record FolderWithFiles(string folder, IReadOnlyList<string> files);
-}
